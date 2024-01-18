5052d3ef2ec02e7ac59ecb743165837f27c11c34
jcode109tech <jedydiahr9@gmail.com>
2024-01-18 21:47:01 +0300

    0x02-Python-import-modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    
+    import sys
+
+    result = 0
+
+    for args in sys.argv:
+        if args != sys.argv[0]:
+            result += int(args)
+            
+    print(result)
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,2 @@
+0x02 python
+this file containes the files used for this specific project for the sprint
