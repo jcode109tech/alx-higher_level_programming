64268fc787c888c7aa78530482db43d5ec191339
jcode109tech <jedydiahr9@gmail.com>
2024-02-15 01:07:44 +0300

    Advanced

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
-def magic_string(iterations=[0]):
-    iterations[0] += 1
-    return "BestSchool" + ", BestSchool" * iterations[0]
+#!/usr/bin/python3
+def magic_string(string=[]):
+    string += ["BestSchool"]
+    return ", ".join(string)
