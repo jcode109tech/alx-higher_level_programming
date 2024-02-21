3e0e22751d1427e4aac75cd19b07db8c091e6a9b
jcode109tech <jedydiahr9@gmail.com>
2024-02-21 22:11:59 +0300

    0x0B-python-input_output

--- 0x00-python-hello_world/101-compile.py
+++ 0x00-python-hello_world/101-compile.py
@@ -1,2 +1,19 @@
 #!/bin/bash
 python3 -m compileall -b $PYFILE
+
+# Check if the PYFILE environment variable is set
+# if [ -z "$PYFILE" ]; then
+#     echo "Error: PYFILE environment variable is not set."
+#     exit 1
+# fi
+
+# # Compile the Python script
+# output_file="${PYFILE}c"
+# echo "Compiling $PYFILE ..."
+# python3 -m compileall "$PYFILE" -b -x __pycache__  # Use -x to exclude __pycache__ directory
+# if [ $? -eq 0 ]; then
+#     echo "Compilation successful. Output file: $output_file"
+# else
+#     echo "Error: Compilation failed."
+#     exit 1
+# fi
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # alx-higher_level_programming
-High level programming
+0x02-python-import_modules
